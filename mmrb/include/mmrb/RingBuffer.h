23a8f885bc60698960ef52151251320a679b2ff5
Cam Giberson <giberson.cameron@gmail.com>
2025-04-23 23:27:21 -0700

    change names of files, make it a library

---
+++ mmrb/include/mmrb/RingBuffer.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+namespace mmrb {
+
+	template <typename T>
+	class RingBuffer {
+	protected:
+		size_t capacity;
+
+	public:
+		RingBuffer(size_t capacity);
+
+		virtual ~RingBuffer() = default;
+
+		bool write(const T& item);
+		bool write(const void* data, size_t size);
+		bool read(T& item);
+		bool read(void* out, size_t size);
+		bool peek(T& item) const;
+		bool empty() const;
+		bool full() const;
+		bool clear();
+	};
+}

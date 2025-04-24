53aa0948266e65c54d0b58a163c6cc819d73384f
Cam Giberson <giberson.cameron@gmail.com>
2025-04-23 23:27:35 -0700

    add basic test console application

---
+++ test/main.cpp
@@ -0,0 +1,16 @@
+#include <mmrb/RingBuffer.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//auto t = new mmrb();
+
+	//cout << t->pop() << endl;
+
+	//delete t;
+
+	return 0;
+}
